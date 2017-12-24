177cfef8d9a3d8f343e85252fc08f0bf21c59cd2
hanemile <emile.hansmaennel@gmail.com>
2017-12-24 14:39:40 +0100

    added the automatic music-mounter to startup.sh

--- starter.sh
+++ starter.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Start Compton for transparency
 ./compton.sh
+
+# Start NetData for monitoring the system
 ./netdata.sh
+
+# Start Redshift for eye-cancer reduction
 ./redshift.sh
+
+# Enable tap-to-click
 ./tap-to-click.sh
+
+# Set a nice wallpaper
 ./wallpaper.sh
+
+# Mount the MPD stick and start mpd
+./music.sh
